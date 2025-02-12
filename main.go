a261910a4295b07dc86083fc2d0769f133552490
kbrice <kbrice@student.42adel.org.au>
2025-02-12 14:01:32 +1030

    Handles server requests to images and css files

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"text/template"
+)
+
+const (
+    srcDir = "content"
+    destDir = "output"
+    tmplDir = "templates"
+    layoutTmpl = "layout.html"
 )
 
 func main() {
     fmt.Println("Setting up webserver...")
+
+    http.Handle("/static/",
+        http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+    /* Resolves server requests to images and css files. */
 
     h1 := func (w http.ResponseWriter, r *http.Request) {
         tmpl := template.Must(template.ParseFiles("index.html"))
@@ -19,6 +30,6 @@ func main() {
 
     log.Fatal(http.ListenAndServe(":8000", nil))
     /* log.Fatal will print out the error if one occurs,
-     * then exit the process. 
+     * then exit the process. Remove log.Fatal in Prod.
      * http.ListenAndServe will setup our webserver on port 8000.*/
 }
