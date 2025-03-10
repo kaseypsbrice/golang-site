e74b30671ea2bbeabcf6c2f652301f0807dfe5c8
kbrice <kbrice@student.42adel.org.au>
2025-03-10 13:38:54 +1030

    Drafted homepage and navbar

--- main.go
+++ main.go
@@ -15,11 +15,7 @@ const (
 )
 
 func handleTemplates(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseFiles(
-        "index.html",
-        "templates/navbar.html",
-        "templates/home.html",
-    ))
+    tmpl := template.Must(template.ParseGlob("index.html, templates/*.html"))
     err := tmpl.ExecuteTemplate(w, "base", nil)
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
