d4e37d51ba0cc8c4e77df9ea63283d8daf57aca7
kbrice <kbrice@student.42adel.org.au>
2025-03-15 16:51:11 +1030

    Restore previous template parsing

--- main.go
+++ main.go
@@ -7,33 +7,30 @@ import (
 	"net/http"
 )
 
-const (
-    srcDir = "content"
-    destDir = "output"
-    tmplDir = "templates"
-    layoutTmpl = "layout.html"
-)
-
 func handleTemplates(w http.ResponseWriter, r *http.Request) {
-    tmpl := template.Must(template.ParseGlob("index.html, templates/*.html"))
-    err := tmpl.ExecuteTemplate(w, "base", nil)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	tmpl := template.Must(template.ParseFiles(
+		"index.html",
+		"templates/navbar.html",
+		"templates/home.html",
+	))
+	err := tmpl.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func main() {
-    fmt.Println("Starting server on :8000...")
+	fmt.Println("Starting server on :8000...")
 
-    http.Handle("/static/",
-        http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
-    /* Resolves server requests to static files e.g. images, css files. */
+	http.Handle("/static/",
+		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	/* Resolves server requests to static files e.g. images, css files. */
 
-    http.HandleFunc("/", handleTemplates)
-    /* Maps a URL pattern to a handler. */
+	http.HandleFunc("/", handleTemplates)
+	/* Maps a URL pattern to a handler. */
 
-    log.Fatal(http.ListenAndServe(":8000", nil))
-    /* log.Fatal will print out the error if one occurs,
-     * then exit the process. Remove log.Fatal in Prod.
-     * http.ListenAndServe will setup our webserver on port 8000.*/
+	log.Fatal(http.ListenAndServe(":8000", nil))
+	/* log.Fatal will print out the error if one occurs,
+	 * then exit the process. Remove log.Fatal in Prod.
+	 * http.ListenAndServe will setup our webserver on port 8000.*/
 }
